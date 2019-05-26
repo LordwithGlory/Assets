49390dd5a9770cc60fa2ae683843572acfda5115
LordwithGlory <1220891359@qq.com>
2019-05-26 18:01:15 +0800

    Initial commit

---
+++ Script/Land.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Land : MonoBehaviour
+{
+    private string uistr = "HELLO";
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnMouseDown()
+    {
+        // 加入一个碰撞器之后才可以 BOX COLINDER 2D
+        Destroy(this.gameObject);
+        //gameObject.GetComponent<Renderer>().enabled = false;
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(gameObject.transform.position.x, gameObject.transform.position.y, 200, 20), uistr);
+    }
+}
---
+++ Script/player3.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player3 : MonoBehaviour
+{
+    //在场景中鼠标点击地面后，角色可以移动到目标位置
+
+    private Vector3 screenPos;
+    private Vector3 mousePos;
+    private Vector3 mousePosInw;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            mouseFollow();
+        }
+    }
+
+    void mouseFollow()
+    {
+        screenPos = Camera.main.WorldToScreenPoint(transform.position);
+        mousePos = Input.mousePosition;
+        mousePos.z = screenPos.z;
+        mousePosInw = Camera.main.ScreenToWorldPoint(mousePos);
+        transform.position = mousePosInw;
+    }
+}
