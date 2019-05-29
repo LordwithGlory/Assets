725944c6e0cddd88fd991a4648c4489045a2c7ed
LordwithGlory <1220891359@qq.com>
2019-05-29 23:00:09 +0800

    0529修改
    能在物体上面显示诗句了还没乱码
    在多个物体上显示诗句

--- Script/Land.cs
+++ Script/Land.cs
@@ -3,39 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Land : MonoBehaviour
+namespace MyUnity
 {
-    private string uistr = "hello world";
-    public Text nameLabel;
-    private Vector3 screenPos;
-    // Start is called before the first frame update
-    void Start()
+    public class Land : MonoBehaviour
     {
-        nameLabel.text = "HELLO";
-    }
+        private string uistr = "hello world";
+        public Text nameLabel;
+        private Vector3 screenPos;
+        // Start is called before the first frame update
+        void Start()
+        {
+            while (MyUnity.Global.guestStc == "") ;
+            Debug.Log(MyUnity.Global.pos);
+            nameLabel.text = MyUnity.Global.guestStc.Substring(MyUnity.Global.pos--, 1);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector3 namePos = Camera.main.WorldToScreenPoint(this.transform.position);
-        namePos.z = 0;
-        nameLabel.transform.position = namePos;
-        
-    }
+        // Update is called once per frame
+        void Update()
+        {
+            Vector3 namePos = Camera.main.WorldToScreenPoint(this.transform.position);
+            namePos.z = 0;
+            nameLabel.transform.position = namePos;
 
-    void OnMouseDown()
-    {
-        // 加入一个碰撞器之后才可以 BOX COLINDER 2D
-        nameLabel.text = "";
-        Destroy(this.gameObject);
-        //gameObject.GetComponent<Renderer>().enabled = false;
-        gameObject.GetComponent<Renderer>().material.color = Color.green;
-    }
+        }
 
-    void OnGUI()
-    {
-        screenPos = Camera.main.WorldToScreenPoint(transform.position);
-        // 这个地方可以设置文字出现的位置, 现在出现了遮挡
-        GUI.Label(new Rect(screenPos.x, screenPos.y, 200, 20), uistr);
+        void OnMouseDown()
+        {
+            // 加入一个碰撞器之后才可以 BOX COLINDER 2D
+            nameLabel.text = "";
+            Destroy(this.gameObject);
+            //gameObject.GetComponent<Renderer>().enabled = false;
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
+        }
+
+        void OnGUI()
+        {
+            screenPos = Camera.main.WorldToScreenPoint(transform.position);
+            // 这个地方可以设置文字出现的位置, 现在出现了遮挡
+            GUI.Label(new Rect(screenPos.x, screenPos.y, 200, 20), uistr);
+        }
     }
 }
--- Script/player3.cs
+++ Script/player3.cs
@@ -2,95 +2,98 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Text;
 using System.IO;
 
-public class player3 : MonoBehaviour
+namespace MyUnity
 {
-    //在场景中鼠标点击地面后，角色可以移动到目标位置
-
-    private Vector3 screenPos;
-    private Vector3 mousePos;
-    private Vector3 mousePosInw;
-    void Start()
+    public class player3 : MonoBehaviour
     {
-        Global.setuptitle();
-        Global.readpoetry();
-    }
+        //在场景中鼠标点击地面后，角色可以移动到目标位置
 
-    void Update()
-    {
-        if (Input.GetButtonDown("Fire1"))
+        private Vector3 screenPos;
+        private Vector3 mousePos;
+        private Vector3 mousePosInw;
+        void Start()
         {
-            mouseFollow();
+            Global.setuptitle();
+            Global.readpoetry();
+            Debug.Log(Global.poeteryTitle);
         }
-    }
 
-    void mouseFollow()
-    {
-        screenPos = Camera.main.WorldToScreenPoint(transform.position);
-        mousePos = Input.mousePosition;
-        mousePos.z = screenPos.z;
-        mousePosInw = Camera.main.ScreenToWorldPoint(mousePos);
-        transform.position = mousePosInw;
-    }
-}
-
-public static class Global
-{
-    // 诗的编号
-    private static int poetrynum;
-    // 分配文字时候确定在句子中的位置（包括混淆文字）
-    public static int pos;
-    private static string[] poetTitles = { "静夜思", "咏鹅", "望岳" };
-    public static string poeteryTitle = "";
-    public static string hintStc = "";
-    // 从这个里读文字，存在混淆的文字
-    public static string guestStc = "";
-    // 正确的诗句
-    public static string crtsen = "";
-
-    public static void setuptitle()
-    {
-        // 暂时设置有3首古诗可以进行选择
-        System.Random rand = new System.Random();
-        poetrynum = rand.Next(1, 3);
-        poeteryTitle = poetTitles[poetrynum];
-    }
-
-    public static void readpoetry()
-    {
-        string filename = poeteryTitle + ".txt";
-        try
+        void Update()
         {
-            // 默认就是utf-8编码所以不需要单独写编码格式
-            using(StreamReader sr=new StreamReader(filename))
+            if (Input.GetButtonDown("Fire1"))
             {
-                // 准备设计第一行是这首诗标题和作者
-                string line;
-                int linenum=0;
-                System.Random thesen = new System.Random();
-                // 找一行进行试错
-                while ((line=sr.ReadLine())!=null)
-                {
-                    linenum++;
-                    if (linenum == 1)
-                    {
-                        poeteryTitle = line;
-                        continue;
-                    }
-                    if (linenum == thesen.Next(2, 5) || linenum == 5)
-                    {
-                        guestStc = line;
-                        break;
-                    }
-                }
-                pos = guestStc.Length - 1;
-                crtsen = guestStc;
+                mouseFollow();
             }
         }
-        catch (Exception e)
+
+        void mouseFollow()
         {
-            Console.WriteLine(e.Data);
+            screenPos = Camera.main.WorldToScreenPoint(transform.position);
+            mousePos = Input.mousePosition;
+            mousePos.z = screenPos.z;
+            mousePosInw = Camera.main.ScreenToWorldPoint(mousePos);
+            transform.position = mousePosInw;
+        }
+    }
+
+    public static class Global
+    {
+        // 诗的编号
+        private static int poetrynum;
+        // 分配文字时候确定在句子中的位置（包括混淆文字）
+        public static int pos;
+        private static string[] poetTitles = { "静夜思", "咏鹅", "望岳" };
+        public static string poeteryTitle = "";
+        public static string hintStc = "";
+        // 从这个里读文字，存在混淆的文字
+        public static string guestStc = "";
+        // 正确的诗句
+        public static string crtsen = "";
+        // 玩家收集的诗句,从空到有
+        public static string collectSen = "";
+
+        public static void setuptitle()
+        {
+            // 暂时设置有3首古诗可以进行选择
+            System.Random rand = new System.Random();
+            poetrynum = rand.Next(0, 0);
+            poeteryTitle = poetTitles[poetrynum];
+        }
+
+        public static void readpoetry()
+        {
+            string filename = poeteryTitle + ".txt";
+            Debug.Log(filename);
+            // 默认就是utf-8编码所以不需要单独写编码格式
+            // 文件需要在根目录下面，需要system.text这个命名空间
+            StreamReader sr = new StreamReader(filename, Encoding.GetEncoding("gb2312"));
+            string line;
+            int linenum = 0;
+            System.Random thesen = new System.Random();
+            // 保证是四行大锅
+            int crtline = thesen.Next(2, 5);
+            while ((line = sr.ReadLine()) != null)
+            {
+                Debug.Log(line);
+                linenum++;
+                if (linenum == 1)
+                {
+                    poeteryTitle = line;
+                    continue;
+                }
+                if (linenum == crtline)
+                {
+                    guestStc = line;
+                    break;
+                }
+            }
+            pos = guestStc.Length - 1;
+            Debug.Log(pos);
+            crtsen = guestStc;
+            guestStc += "某人亮";
         }
     }
 }
